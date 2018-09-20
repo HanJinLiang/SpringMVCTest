19a088beb2d2b8415b696fce0dbb5ba6ef0b07f7
HanJinLiang <hanjinliang1992@163.com>
2018-09-21 00:18:52 +0800

    Initial commit

---
+++ src/test/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/mvc")
+public class HelloWorldController {
+
+    @RequestMapping(value = "/hello" ,method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "Hello ooo";
+    }
+}
